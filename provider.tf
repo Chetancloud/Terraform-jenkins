f82dbe7ec6364de15971fe2f5cd50019f0a189a7
EC2 Default User <ec2-user@ip-172-31-43-206.eu-west-1.compute.internal>
2022-10-13 11:32:43 +0000

    Updated provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_instance" "first AWS instance" {
+resource "aws_instance" "first-AWS-instance" {
   ami           = "ami-096800910c1b781ba"
   instance_type = "t2.micro"
 }
